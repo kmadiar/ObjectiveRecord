5fb7d692c1f15a968f69bb6ae50bbbf6427110ed
Kostiantyn Madiar <>
2019-08-02 00:31:55 +0300

    changed bundle

--- Classes/CoreDataManager.m
+++ Classes/CoreDataManager.m
@@ -47,7 +47,7 @@
 #pragma mark - Private
 
 - (NSString *)appName {
-    return [[NSBundle bundleForClass:[self class]] infoDictionary][@"CFBundleName"];
+    return [[NSBundle mainBundle] infoDictionary][@"CFBundleName"];
 }
 
 - (NSString *)databaseName {
@@ -80,7 +80,7 @@
 - (NSManagedObjectModel *)managedObjectModel {
     if (_managedObjectModel) return _managedObjectModel;
 
-    NSURL *modelURL = [[NSBundle bundleForClass:[self class]] URLForResource:[self modelName] withExtension:@"momd"];
+    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:[self modelName] withExtension:@"momd"];
     _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
     return _managedObjectModel;
 }
